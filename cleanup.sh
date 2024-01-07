303577d04687191572644bbb484cb4c2b5377669
sanka <a.sotov@yahoo.co.uk>
2024-01-07 13:30:29 +0100

    minor changes

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-md_files="/home/runko/polona/content/*.md"
-
-for file in $md_files; do
-  # Remove lines containing '!!int' or '!!null' or "'Tags': []"
-  sed -i -e 's/\/[^ ]* \/ [^ ]*\//-/g' -e 's/\/[^ ]*\/[^ ]*\//-/g' $file
-done
-
